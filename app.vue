318f8e7a3d117cb46fe9f2aa034fa0c445b8c665
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2025-01-01 19:32:07 +0300

    Анимация смены роута

--- app.vue
+++ app.vue
@@ -1,9 +1,21 @@
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <Transition name="fade" mode="out-in">
+      <NuxtPage />
+    </Transition>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
