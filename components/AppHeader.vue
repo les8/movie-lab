99a6649cbed6d377d2812300ad8e37fece5e4920
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2025-01-01 18:39:56 +0300

    Основа header

---
+++ components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h1>Test Task</h1>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+header {
+  position: sticky;
+  top: 0;
+  left: 0;
+}
+</style>
