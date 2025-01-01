f574afaee900655a2f23a10d01c4a2372ea5ff63
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2025-01-01 19:27:05 +0300

    Дефолтный лайаут

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="layout">
+    <AppHeader />
+    <slot />
+    <AppFooter />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.layout {
+  position: relative;
+}
+</style>
