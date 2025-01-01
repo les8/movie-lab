b55e2ad53b3dc17987c6cabb8972ca0540630ee6
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2025-01-01 18:03:52 +0300

    Добавил основные стили, шрифт

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/scss/main.scss'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap',
+        },
+      ],
+    },
+  },
 })
