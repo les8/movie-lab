0bb9e832798b7fdf22f6921201e1486013da5f24
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2025-01-01 19:26:23 +0300

    Футер приложения

---
+++ components/AppFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <h5>Test Task, 2022</h5>
+
+    <address>
+      <p class="phone">Телефон: <a href="tel:+5183632171">518-363-2171</a></p>
+      <p>91 Park St. Fairhope, AL 36532</p>
+    </address>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.phone {
+  margin-bottom: 4px;
+}
+</style>
