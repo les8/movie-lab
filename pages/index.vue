ffdd2aa2fc9066a50ee83fe21964c63bdaee0dc0
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2025-01-04 09:10:54 +0300

    Адаптив и вывод списка фильмов

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,36 @@
 <template>
   <main class="movies">
-    <ul class="movies__list"></ul>
+    <ul class="movies__list">
+      <TheMovie v-for="movie in movies" :key="movie.id" />
+    </ul>
   </main>
 </template>
 
 <script lang="ts" setup>
+import TheMovie from '~/components/movie/TheMovie.vue';
+
 interface Movie {
-  description: string
-  duration: string
-  genre: string
-  id: number
-  name: string
-  poster: string
-  rating: number
+  description: string;
+  duration: string;
+  genre: string;
+  id: number;
+  name: string;
+  poster: string;
+  rating: number;
+}
+
+interface Result {
+  items: Movie[];
 }
 
 const movies = ref<Movie[]>([]);
 const getMovies = async (): Promise<void> => {
   try {
-    movies.value = await $fetch('api/data', {
+    const result = await $fetch<Result>('api/data', {
       method: 'GET',
     });
+
+    movies.value = result.items;
   } catch (e) {
     console.error(e);
   }
@@ -33,5 +43,24 @@ getMovies();
 .movies {
   margin: 0 auto;
   padding: 32px 48px;
+  max-width: 1440px;
+
+  &__list {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 16px;
+
+    @media (min-width: 768px) {
+      grid-template-columns: 1fr 1fr;
+    }
+
+    @media (min-width: 992px) {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+
+    @media (min-width: 1200px) {
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+  }
 }
 </style>
