4add2dda9dc636de256de590196f8a7ca017e9d4
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2025-01-05 14:23:23 +0300

    Запрос к деталям фильма

--- pages/movie/[id].vue
+++ pages/movie/[id].vue
@@ -2,6 +2,17 @@
   <div></div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="js" setup>
+const { data: details, error } = await useAsyncData(
+  'movieDetails',
+  () => $fetch(`/api/detail`, {
+    method: 'GET'
+  })
+);
+
+if (error.value) {
+  console.error(error.value);
+}
+</script>
 
 <style lang="scss" scoped></style>
