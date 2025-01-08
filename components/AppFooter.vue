cd3f5bca84eea8c914a5a685ab762184751da1d7
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2025-01-08 17:00:41 +0300

    Актуализировал стили

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,9 +1,9 @@
 <template>
-  <footer class="sticky top-full left-0 py-8 px-12">
-    <div class="max-w-screen-2xl w-full mx-auto 2xl:px-12 flex justify-between">
+  <footer class="sticky top-full left-0 py-8 px-12 bg-slate-800">
+    <div class="max-w-screen-2xl w-full mx-auto 2xl:px-12 flex justify-between items-center">
       <h5>Movie Lab, 2025</h5>
 
-      <address>
+      <address class="not-italic">
         <p class="mb-1">Телефон: <a href="tel:+5183632171">518-363-2171</a></p>
         <p>91 Park St. Fairhope, AL 36532</p>
       </address>
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="sticky top-0 left-0 z-20 py-8 px-12">
+  <header class="sticky top-0 left-0 z-20 py-8 px-12 bg-slate-800">
     <nav class="max-w-screen-2xl w-full mx-auto 2xl:px-12">
       <NuxtLink to="/">
         <h1>Movie Lab</h1>
