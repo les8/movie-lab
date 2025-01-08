810b766909602e496f2093dea4d52df7f83355c2
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2025-01-08 17:00:16 +0300

    Актуализировал дефолтные стили

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/scss/main.scss'],
+  css: ['~/assets/css/tailwind.css'],
   app: {
     head: {
       link: [
