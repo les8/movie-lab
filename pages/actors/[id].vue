9f5265b577a84b094e571121516f020c1cbc4b1c
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2025-01-08 17:42:14 +0300

    Стилизовал страницу с актером

--- pages/actors/[id].vue
+++ pages/actors/[id].vue
@@ -1,5 +1,5 @@
 <template>
-  <h2>actor {{ useRoute().params.id }}</h2>
+  <h2 class="py-6 max-w-screen-2xl w-full mx-auto px-12">actor {{ useRoute().params.id }}</h2>
 </template>
 
 <script setup lang="ts">
